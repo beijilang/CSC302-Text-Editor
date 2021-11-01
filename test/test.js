148785304faea60091477771372e069604213542
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-10-31 21:09:09 -0400

    Remove unused 'tests' and shortcut JSON

--- test/test.js
+++ test/test.js
@@ -1,4 +1,1 @@
-import TextEditor from "../src/TextEditor.js";
-
-const editor = new TextEditor();
-editor.init("CSC302.txt", "r");
+// stub
--- test/testDav.js
+++
@@ -1,5 +0,0 @@
-import termKit from "terminal-kit";
-import TextEditor from "../src/TextEditor.js";
-
-const editor = new TextEditor();
-editor.init();
