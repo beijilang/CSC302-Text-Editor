839c5f5f84a759904b3b812cca8a56e2b6839560
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-12-05 00:01:48 -0500

    test: linked list

---
+++ test/undo-redo.spec.js
@@ -0,0 +1,57 @@
+import fs from "fs";
+import pty from "node-pty";
+import { SnapShotLinkedListNode, TextEditorStateManagementLinkList } from "../src/ObjectState";
+
+const sleep = interval => new Promise(resolve => setTimeout(resolve, interval));
+
+describe("Command linked list (in-process)", () => {
+    test("node constructor", () => {
+        const node = new SnapShotLinkedListNode();
+        expect(node.previous).toBeNull();
+        expect(node.next).toBeNull();
+    });
+    test("list constructor", () => {
+        const list = new TextEditorStateManagementLinkList(null, null);
+        expect(list.get_cur_node()).toBeNull();
+    });
+    test("sequential insert", () => {
+        const one = new SnapShotLinkedListNode();
+        const two = new SnapShotLinkedListNode();
+        const three = new SnapShotLinkedListNode();
+        const list = new TextEditorStateManagementLinkList(one, null);
+        list.insertNewState(two);
+        list.insertNewState(three);
+        expect(list.get_cur_node()).toBe(three);
+        expect(list.get_last_action()).toBe(two);
+        expect(list.get_next_action()).toBeNull();
+        expect(one.previous).toBeNull();
+        expect(one.next).toBe(two);
+        expect(two.previous).toBe(one);
+        expect(two.next).toBe(three);
+        expect(three.previous).toBe(two);
+        expect(three.next).toBeNull();
+    })
+    test("traverse history", () => {
+        const head = new SnapShotLinkedListNode();
+        const undone = new SnapShotLinkedListNode();
+        const replacement = new SnapShotLinkedListNode();
+        const list = new TextEditorStateManagementLinkList(head, null);
+        list.insertNewState(undone);
+        expect(list.get_cur_node()).toBe(undone);
+        list.move_cur_node_to_left();
+        expect(list.get_cur_node()).toBe(head);
+        expect(list.get_next_action()).toBe(undone);
+        list.move_cursor_to_right();
+        expect(list.get_cur_node()).toBe(undone);
+        expect(list.get_next_action()).toBeNull();
+        list.move_cur_node_to_left();
+        list.insertNewState(replacement);
+        expect(list.get_cur_node()).toBe(replacement);
+        expect(list.get_last_action()).toBe(head);
+        expect(list.get_next_action()).toBeNull();
+    });
+});
+
+describe("Undo and redo (pseudo-TTY child process)", () => {
+
+});
