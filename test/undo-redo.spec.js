370d19ea2cca03bd62b136cf61c23afd6576a1cb
kevinlul <6320810+kevinlul@users.noreply.github.com>
2021-12-05 00:22:27 -0500

    test: undo-redo in practice

--- test/undo-redo.spec.js
+++ test/undo-redo.spec.js
@@ -2,8 +2,6 @@ import fs from "fs";
 import pty from "node-pty";
 import { SnapShotLinkedListNode, TextEditorStateManagementLinkList } from "../src/ObjectState";
 
-const sleep = interval => new Promise(resolve => setTimeout(resolve, interval));
-
 describe("Command linked list (in-process)", () => {
     test("node constructor", () => {
         const node = new SnapShotLinkedListNode();
@@ -52,6 +50,29 @@ describe("Command linked list (in-process)", () => {
     });
 });
 
-describe("Undo and redo (pseudo-TTY child process)", () => {
+const sleep = interval => new Promise(resolve => setTimeout(resolve, interval));
+const result = () => fs.readFileSync("undo.txt", { encoding: "utf8" });
 
+describe("Undo and redo (pseudo-TTY child process)", () => {
+    let process;
+    beforeEach(async () => {
+        fs.writeFileSync("undo.txt", "");
+        process = pty.spawn("node", ["src", "undo.txt"]);
+        await sleep(400);
+    });
+    afterEach(() => {
+        process.kill();
+    });
+    test("example 1", async () => {
+        expect.assertions(1);
+        process.write("123456\x1A\x1A\x19\x13\x03"); // Ctrl+Z, Ctrl+Z, Ctrl+Y, Ctrl+S, Ctrl+C
+        await sleep(400);
+        expect(result()).toEqual("12345");
+    });
+    test("example 2", async () => {
+        expect.assertions(1);
+        process.write("123456\x1A\x1A7\x1A\x19\x13\x03"); // Ctrl+Z, Ctrl+Z, 7, Ctrl+Z, Ctrl+Y, Ctrl+S, Ctrl+C
+        await sleep(400);
+        expect(result()).toEqual("12347");
+    });
 });
